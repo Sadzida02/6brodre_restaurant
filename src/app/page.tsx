f728e11d41bfc29b3c53eacff874f9344230a5ab
Sadžida Valjevac <valjevac.sadzida02@gmail.com>
2024-08-02 23:27:57 +0200

    dishCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,8 @@ function page() {
   return (
     <div className='w-full h-928'>
       <h1 className="text-44 font-extrabold p-5 font-arima" >6 brodre restaurant</h1>
-
-      <div className='h-620 w-530 border-white border-8 shadow-lg rounded-md flex flex-col p-4 mx-64'>
-
-        <h2 className='text-2xl my-4'>Chicken Steak</h2>
-        <p>Chicken Steak Recipe With Pan Roasted Vegetables & Potato Mash is a delicious meal in itself. The chicken is marinated in a lovely marinade that is bursting with flavours that include fresh parsley , lemon, garlic and mixed herbs. All of which add to the yummilicious taste of the chicken. </p>
-
-      </div>
+      <DishCard />
+      
     </div>
   )
 }
